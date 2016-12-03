ebb8235b3bd7f6458109e96ba2acc5888d30919b
Christoph <christoph.gnip@gmail.com>
2016-12-03 14:58:39 +0100

    Solve Task 3 and 4

--- wiki/web/controllers/page_controller.ex
+++ wiki/web/controllers/page_controller.ex
@@ -2,10 +2,12 @@ defmodule Wiki.PageController do
   use Wiki.Web, :controller
 
   def index(conn, _params) do
-    pages = [
-      %{title: "Main Page", body: "Welcome"},
-    ]
+    pages = Repo.all(Wiki.Page)
     render conn, "index.html", pages: pages
   end
 
+  def show(conn, %{"id" => id}) do
+    page = Repo.get_by!(Wiki.Page, title: id)
+    render conn, "show.html", page: page
+  end
 end
