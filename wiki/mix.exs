cc13f0a0a96fe112b0acc3bb99cba8d354426161
Christoph <christoph.gnip@gmail.com>
2016-12-04 11:18:55 +0100

    :sparkles: implement markdown renderer

--- wiki/mix.exs
+++ wiki/mix.exs
@@ -19,7 +19,7 @@ defmodule Wiki.Mixfile do
   def application do
     [mod: {Wiki, []},
      applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex]]
+                    :phoenix_ecto, :postgrex, :earmark,]]
   end
 
   # Specifies which paths to compile per environment.
@@ -30,14 +30,15 @@ defmodule Wiki.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.2.0"},
+    [{:phoenix, "~> 1.2.1"},
      {:phoenix_pubsub, "~> 1.0"},
      {:phoenix_ecto, "~> 3.0"},
      {:postgrex, ">= 0.0.0"},
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:earmark, "~> 1.0"}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
--- wiki/web/views/page_view.ex
+++ wiki/web/views/page_view.ex
@@ -1,3 +1,11 @@
 defmodule Wiki.PageView do
   use Wiki.Web, :view
+
+  def wiki_to_html(nil), do: ""
+  def wiki_to_html(markdown) do
+    markdown
+    |> String.replace(~r/\[\[([\w|\s]+)\]\]/, "[\\1](/\\1)")
+    |> Earmark.to_html()
+    |> raw()
+  end
 end
